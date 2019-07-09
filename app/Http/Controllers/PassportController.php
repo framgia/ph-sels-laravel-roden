00bb39543cc476aaeaaf8559ca636f2c8071690a
Roden Artuyo <roden.artuyo@sun-asterik.com>
2019-07-09 14:35:53 +0800

    [SELS-TASK] Implement Authentication and routing

---
+++ app/Http/Controllers/PassportController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PassportController extends Controller
+{
+    /**
+     * Handles Registration Request
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(Request $request)
+    {
+
+        $this->validate($request, [
+            'name' => 'required|min:3',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        $token = $user->createToken('sels')->accessToken;
+
+        return response()->json(['token' => $token , 'user' => $user], 200);
+    }
+
+    /**
+     * Handles Login Request
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+
+        if (auth()->attempt($credentials)) {
+            $token = auth()->user()->createToken('sels')->accessToken;
+            return response()->json(['token' => $token , 'user' => auth()->user()], 200);
+        } else {
+            return response()->json(['errorMsg' => 'Incorrect Password or Email'], 401);
+        }
+    }
+
+    /**
+     * Returns Authenticated User Details
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function details()
+    {
+        return response()->json(['user' => auth()->user()], 200);
+    }
+
+    /**
+     * Returns
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout(){
+
+        $user = Auth::user()->token();
+        $user->revoke();
+
+        return response()->json(['message' => 'Logout Successfully']);
+    }
+
+}
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -30,11 +30,17 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  */
 
 let token = document.head.querySelector('meta[name="csrf-token"]');
+let auth_token = localStorage.getItem('access_token');
 
 if (token) {
     window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 } else {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
+
+
+if(auth_token){
+	window.axios.defaults.headers.common['Authorization'] = 'Bearer '+ auth_token;
 }
 
 /**
